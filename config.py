76e42bba230963d509dd91cc58d5b06b8208eb1f
nasFold <117340842+nasFold@users.noreply.github.com>
2024-01-07 23:08:19 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,6 @@
 # config.py
 import os
 import openai
-
-DISCORD_BOT_TOKEN = 'MTE5MzQ5OTczMzA4ODU0Njg5Nw.GW7PCA.ne3rT5oSMnkZfCDPQCU_z3wMpaCpYgM-OzhpnY'
-
 # WeatherAPI key
 WEATHER_API_KEY = '443f8ce319ad4c4ba27164818240501'
 
